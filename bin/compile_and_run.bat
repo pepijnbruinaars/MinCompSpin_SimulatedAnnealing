9fb93f6834d20721c247ec6e0c46ceb91d00d6e9
ebokai <ebokai@hotmail.com>
2023-02-22 14:47:25 +0100

    move batch files to main folder

--- bin/compile_and_run.bat
+++
@@ -1,3 +0,0 @@
-g++ -std=c++11 -O3 -Wall ../src/*.cpp -o saa.exe
-saa.exe my_data_n100_N2000 -l my_comms_n100
-pause
---
+++ compile_and_run.bat
@@ -0,0 +1,4 @@
+g++ -std=c++11 -O3 -Wall ./src/*.cpp -o ./bin/saa.exe
+cd bin
+saa.exe my_data_n100_N2000 -l my_comms_n100
+pause
